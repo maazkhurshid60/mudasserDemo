58d0ccb7b00d19ca57f9b0b12d12b64bc310f5e0
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-08 16:51:43 +0500

    replace  new name

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <div><p>Mr Ali Raza Bhatti</p></div>
+      <div><p>Mr Hamza Ishtiaq</p></div>
     </>
   )
 }
 
 export default App
+
