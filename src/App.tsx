883284c8cbaf6f3eb2ec4491859ac4d374bdf908
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-08 18:53:50 +0500

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-
-
-
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-
-      <div><p>Mr Danial Pasha</p>
-      <p>Mr Hassan</p>
-      <p>Mr Noor Ul Hassan</p>
-
-      <div><p>Mr Danial Pasha</p>
-      <p>Mr Hassan</p>
-      <p>Mr Noor Ul Hassan</p>
+      <div>
+        <p>Mr Danial Pasha</p>
+        <p>Mr Hassan</p>
+        <p>Mr Noor Ul Hassan</p>
       </div>
 
-      <div><p>Mr HamzaIshtiaq</p></div>
+      <div>
+        <p>Mr Danial Pasha</p>
+        <p>Mr Hassan</p>
+        <p>Mr Noor Ul Hassan</p>
+      </div>
 
+      <div>
+        <p>Mr Hamza Ishtiaq</p>
+      </div>
 
-      <div><p>Mr Ali Raza Bhatti</p></div>
+      <div>
+        <p>Mr Ali Raza Bhatti</p>
+      </div>
 
-      <div><p>Mr Ali Raza Bhatti</p></div>
-      <div><p>Mr Hamza Ishtiaq</p></div>
+      <div>
+        <p>Mr Ali Raza Bhatti</p>
+      </div>
+
+      <div>
+        <p>Mr Hamza Ishtiaq</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
